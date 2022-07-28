f0756781c1d277149ad4af2937cb245a772e1eb5
“PedroMCHenrique” <pedro.876.ph@gmail.com>
2022-07-28 17:51:37 -0300

    Feature: add register route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express';
-import LoginController from '../controllers/LoginController';
-import LoginService from '../services/LoginService';
+import LoginController from '../controllers/UserController';
+import UserService from '../services/UserService';
 import TokenGenerator from '../helpers/TokenGenerator';
 import LoginValidator from '../validators/LoginValidator';
+import UserValidator from '../validators/UserValidator';
 
 const loginValidator = new LoginValidator();
+const userValidator = new UserValidator();
 const tokenGenerator = new TokenGenerator();
-const loginService = new LoginService(loginValidator);
-const loginController = new LoginController(loginService, tokenGenerator);
+const userService = new UserService(loginValidator, userValidator);
+const userController = new LoginController(userService, tokenGenerator);
 
 const router = Router();
 
 router.post('/login', async (req, res, next) => {
-  return loginController.login(req, res, next);
+  return userController.login(req, res, next);
+})
+
+router.post('/register',async (req, res, next) => {
+  return userController.register(req, res, next);
 })
 
 export default router;
