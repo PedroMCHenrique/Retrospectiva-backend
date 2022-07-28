79b6106b21aa4165ed81fc79b6bf83b0769b9aef
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-07-28 18:26:29 -0300

    feat: add getById Cableways service

--- backend/src/services/CablewayService.ts
+++ backend/src/services/CablewayService.ts
@@ -1,4 +1,5 @@
 import CablewayModel from "../database/models/CablewayModel";
+import HandleError from "../helpers/HandleError";
 import { ICableway } from "../interfaces";
 
 class CablewayService {
@@ -6,7 +7,16 @@ class CablewayService {
     const cableways = await CablewayModel.findAll();
     return cableways;
   }
-  
+
+  async getById(id: string): Promise<ICableway> {
+    const cableways = await CablewayModel.findOne({ where: { id: id } });
+
+    if (!cableways) {
+      throw HandleError.notFound(`Cableway #${id} not found`)
+    }
+
+    return cableways;
+  }
 }
 
 export default CablewayService;
