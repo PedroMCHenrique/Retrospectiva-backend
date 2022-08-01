17cecbfb35406f5dd78f1cb1fb8835067e570353
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-08-01 13:48:12 -0300

    feat: add getById Cableways controller

--- backend/src/controllers/CablewayController.ts
+++ backend/src/controllers/CablewayController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import CablewayService from "../services/CablewayService";
 
 class CablewayController {
@@ -7,6 +7,16 @@ class CablewayController {
   async getAll(_req: Request, res: Response) {
     const cableways = await this.cablewayService.getAll();
     return res.status(200).json(cableways);
+  }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    try {
+      const cableway = await this.cablewayService.getById(id);
+      res.status(200).json(cableway);
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
