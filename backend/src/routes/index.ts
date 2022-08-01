1774e9390248d26290ad4309d83ac8262316cc9c
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-08-01 13:53:43 -0300

    feat: add Cableways router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,16 +3,28 @@ import LoginController from '../controllers/LoginController';
 import LoginService from '../services/LoginService';
 import TokenGenerator from '../helpers/TokenGenerator';
 import LoginValidator from '../validators/LoginValidator';
+import CablewayController from '../controllers/CablewayController';
+import CablewayService from '../services/CablewayService';
 
 const loginValidator = new LoginValidator();
 const tokenGenerator = new TokenGenerator();
 const loginService = new LoginService(loginValidator);
 const loginController = new LoginController(loginService, tokenGenerator);
+const cablewayService = new CablewayService();
+const cablewayController = new CablewayController(cablewayService);
 
 const router = Router();
 
 router.post('/login', async (req, res, next) => {
   return loginController.login(req, res, next);
+})
+
+router.get('/cableway/:id', async (req, res, next) => {
+  return cablewayController.getById(req, res, next);
+})
+
+router.get('/cableway', async (req, res,) => {
+  return cablewayController.getAll(req, res);
 })
 
 export default router;
