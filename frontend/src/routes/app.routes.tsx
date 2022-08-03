8de32f994bdcdd08726352c1799157c6f94ae86f
“ganovaistrybe” <gabriel.novais@betrybe.com>
2022-08-03 11:27:57 -0300

    [Frontend] - Validação das rotas

--- frontend/src/routes/app.routes.tsx
+++ frontend/src/routes/app.routes.tsx
@@ -2,8 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import { Cableway } from '../screens/Cableway';
 import { Finish } from '../screens/Finish';
 import { Home } from '../screens/Home';
-import { Login } from '../screens/Login';
-import { Register } from '../screens/Register';
 
 export function AppRoutes() {
   return (
@@ -11,8 +9,6 @@ export function AppRoutes() {
       <Route path="/home" element={<Home />} />
       <Route path="/cableway/:id" element={<Cableway />} />
       <Route path="/finish" element={<Finish />} />
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
